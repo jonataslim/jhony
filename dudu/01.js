cf1dd97299e81beb5927d9724f24670ec78a3ea7
jonataslim <jonatas.arante@escolar.ifrn.edu.br>
2024-03-20 15:50:46 -0300

    imcompleto

--- dudu/01.js
+++ dudu/01.js
@@ -1,4 +1,15 @@
 function dudu() {
-    data = document.getElementById('data')
-    
+    let data = document.getElementById('data').value
+    let dataNas = new Date(data)
+    let res = document.getElementById('res')
+
+    let hoje = new Date()
+
+    let idade = hoje.getFullYear() - dataNas.getFullYear
+    let mes = hoje.getMonth() - dataNas.getMonth
+
+    if (mes < 0 || (mes === 0 && hoje.getDate() < dataNas.getDate())) {
+        idade--
+    }
+    res.innerHTML = `vc tem: ${idade}`
 }
